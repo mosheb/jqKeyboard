02b5d91a44d1b784473050d0e94029c22aece2d3
Moshe Borisute <moshe.borisute@gmail.com>
2018-06-27 22:20:07 -0400

    Add files via upload

---
+++ layouts/jqk.layout.he.js
@@ -0,0 +1,19 @@
+/* English */
+var jqKeyboard = jqKeyboard || {};
+
+jqKeyboard.layouts = [{
+    lang: "he",
+    layout: ["1 2 3 4 5 6 7 8 9 0 <<backspace>>",
+            "\u05e7 \u05e8 \u05d0 \u05d8 \u05d5 \u05df \u05dd \u05e4 ( )",
+            "\u05e9 \u05d3 \u05d2 \u05db \u05e2 \u05d9 \u05d7 \u05dc \u05da \u05e3 \" '",
+            "\u05d6 \u05e1 \u05d1 \u05d4 \u05e0 \u05de \u05e6 \u05ea \u05e5",
+            "<<space>> <<left>> <<right>>"]
+},
+	{
+    lang: "en",
+    layout: ["`|~ 1|! 2|@ 3|# 4|$ 5|% 6|^ 7|& 8|* 9|( 0|) -|_ =|+ <<backspace>>",
+            "q w e r t y u i o p [|{ ]|} \\||",
+            "a s d f g h j k l ;|: '|\"",
+            "<<shift>> z x c v b n m ,|< .|> /|? <<shift>>",
+            "<<space>>"]
+}];
